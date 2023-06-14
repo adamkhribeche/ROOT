382a36e836f514efccbd1fd6f4fd254d48ba9204
adamkhribeche <48175472+adamkhribeche@users.noreply.github.com>
2023-06-14 12:17:28 +0100

    end > length fixed

--- slicer.js
+++ slicer.js
@@ -2,17 +2,17 @@ let arr = [0, 3, 4, 6, 6, 6];
 
 function slice(input, start, end = input.length) {
     let output = [];
+    let len = input.length;
 
-    start = start >= 0 ? start : input.length + start;
-    end = end >= 0 ? end : input.length + end;
-    if (end <= start)
-        return [];
+    start = start >= 0 ? start : len + start;
+    end = end >= 0 ? end : len + end;
+    end = end > len ? len : end;
     for (let i = 0; start < end; i++)
         output[i] = input[start++];
     return output;
 }
 
 console.log(arr);
-console.log(slice(arr, 3));
-console.log(arr.slice(3));
-console.log(arr);
+console.log(slice(arr, 2, 1));
+console.log(arr.slice(2, 1));
+// console.log(arr);
